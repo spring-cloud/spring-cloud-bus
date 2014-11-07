2943b385b836901c9f5ec34068337586cef2c35e
Dave Syer <dsyer@pivotal.io>
2014-11-07 18:35:50 +0000

    Make filter based on context ID instead of name
    Also added unit tests and separated AMQP from native
    ApplicationContext event features.
    
    Fixes gh-6

--- src/main/java/org/springframework/cloud/bus/endpoint/EnvironmentBusEndpoint.java
+++ src/main/java/org/springframework/cloud/bus/endpoint/EnvironmentBusEndpoint.java
@@ -1,24 +1,29 @@
 package org.springframework.cloud.bus.endpoint;
 
+import java.util.Map;
+
 import org.springframework.cloud.bus.event.EnvironmentChangeRemoteApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
 
 /**
  * @author Spencer Gibb
  */
 public class EnvironmentBusEndpoint extends AbstractBusEndpoint {
 
+	public EnvironmentBusEndpoint(ApplicationEventPublisher context, String id, BusEndpoint delegate) {
+		super(context, id, delegate);
+	}
+
     @RequestMapping(value = "env", method = RequestMethod.POST)
     @ResponseBody
     //TODO: make this an abstract method in AbstractBusEndpoint?
     public void env(@RequestParam Map<String, String> params,
                         @RequestParam(value = "destination", required = false) String destination) {
-        publish(new EnvironmentChangeRemoteApplicationEvent(this, getAppName(), destination, params));
+        publish(new EnvironmentChangeRemoteApplicationEvent(this, getInstanceId(), destination, params));
     }
 
 
