a598df656dc1da9cd06b559b0b918980870b49e5
Dave Syer <dsyer@pivotal.io>
2014-12-11 12:23:06 +0000

    Add MessageConverter to AMQP endpoints
    So peers can speak JSON instead of Java
    
    Fixes gh-8

--- spring-cloud-bus/src/main/java/org/springframework/cloud/bus/amqp/AmqpBusAutoConfiguration.java
+++ spring-cloud-bus/src/main/java/org/springframework/cloud/bus/amqp/AmqpBusAutoConfiguration.java
@@ -1,5 +1,7 @@
 package org.springframework.cloud.bus.amqp;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.AnonymousQueue;
 import org.springframework.amqp.core.Binding;
@@ -7,6 +9,8 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -18,12 +22,14 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.amqp.Amqp;
 import org.springframework.messaging.MessageChannel;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * @author Spencer Gibb
  */
 @Configuration
 @ConditionalOnClass(AmqpTemplate.class)
-@ConditionalOnExpression("${bus.amqp.enabled:true}")
+@ConditionalOnExpression("${bus.enabled:true} && ${bus.amqp.enabled:true}")
 public class AmqpBusAutoConfiguration {
 
 	public static final String SPRING_CLOUD_BUS = "spring.cloud.bus";
@@ -32,19 +38,27 @@ public class AmqpBusAutoConfiguration {
 	private ConnectionFactory connectionFactory;
 
 	@Autowired
-	private AmqpTemplate amqpTemplate;
+	private RabbitTemplate amqpTemplate;
 
-	// TODO: how to fail gracefully if no rabbit?
+	@Autowired(required = false)
+	private ObjectMapper objectMapper;
+
+	@PostConstruct
+	public void init() {
+		Jackson2JsonMessageConverter converter = messageConverter();
+		amqpTemplate.setMessageConverter(converter);
+	}
+
 	@Bean
 	protected FanoutExchange cloudBusExchange() {
 		// TODO: change to TopicExchange?
 		FanoutExchange exchange = new FanoutExchange(SPRING_CLOUD_BUS);
 		return exchange;
 	}
-	
+
 	@Bean
 	protected Binding localCloudBusQueueBinding() {
-		return BindingBuilder.bind(localCloudBusQueue()).to(cloudBusExchange());		
+		return BindingBuilder.bind(localCloudBusQueue()).to(cloudBusExchange());
 	}
 
 	@Bean
@@ -65,8 +79,17 @@ public class AmqpBusAutoConfiguration {
 	public IntegrationFlow cloudBusAmqpInboundFlow(
 			@Qualifier("cloudBusInboundChannel") MessageChannel cloudBusInboundChannel) {
 		return IntegrationFlows
-				.from(Amqp.inboundAdapter(connectionFactory, localCloudBusQueue()))
+				.from(Amqp.inboundAdapter(connectionFactory, localCloudBusQueue())
+						.messageConverter(messageConverter()))
 				.channel(cloudBusInboundChannel).get();
+	}
+
+	private Jackson2JsonMessageConverter messageConverter() {
+		Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+		if (objectMapper != null) {
+			converter.setJsonObjectMapper(objectMapper);
+		}
+		return converter;
 	}
 
 }
