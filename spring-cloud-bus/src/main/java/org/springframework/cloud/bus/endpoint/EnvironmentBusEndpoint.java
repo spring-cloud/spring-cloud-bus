c2733af9aaeb7bc7c5056a01f81b8a3a6f845ece
Stephen Oakey <stephen.oakey@gmail.com>
2016-12-29 20:43:03 -0500

    Adds JMX Operations to Bus Endpoints
    Adds @ManagedResource and @ManagedOperation to
    the EnvironmentBusEndpoint and RefreshBusEndpoint
    to expose these operations over JMX.
    
    Fixes gh-50

--- spring-cloud-bus/src/main/java/org/springframework/cloud/bus/endpoint/EnvironmentBusEndpoint.java
+++ spring-cloud-bus/src/main/java/org/springframework/cloud/bus/endpoint/EnvironmentBusEndpoint.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.cloud.bus.event.EnvironmentChangeRemoteApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @author Spencer Gibb
  */
+@ManagedResource
 public class EnvironmentBusEndpoint extends AbstractBusEndpoint {
 
 	public EnvironmentBusEndpoint(ApplicationEventPublisher context, String id,
@@ -21,6 +24,7 @@ public class EnvironmentBusEndpoint extends AbstractBusEndpoint {
 
 	@RequestMapping(value = "env", method = RequestMethod.POST)
 	@ResponseBody
+	@ManagedOperation
 	// TODO: make this an abstract method in AbstractBusEndpoint?
 	public void env(@RequestParam Map<String, String> params,
 			@RequestParam(value = "destination", required = false) String destination) {
