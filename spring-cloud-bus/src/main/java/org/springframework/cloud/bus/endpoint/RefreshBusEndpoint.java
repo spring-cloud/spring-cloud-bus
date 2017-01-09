5aad681d3f4c0c68247ac7ad3cd6e8bc95a41435
Steve Oakey <stephen.oakey@gmail.com>
2017-01-09 13:36:24 -0500

    Adds JMX Operations to Bus Endpoints (#52)
    Adds @ManagedResource and @ManagedOperation to
    the EnvironmentBusEndpoint and RefreshBusEndpoint
    to expose these operations over JMX.
    
    Fixes gh-50

--- spring-cloud-bus/src/main/java/org/springframework/cloud/bus/endpoint/RefreshBusEndpoint.java
+++ spring-cloud-bus/src/main/java/org/springframework/cloud/bus/endpoint/RefreshBusEndpoint.java
@@ -2,6 +2,9 @@ package org.springframework.cloud.bus.endpoint;
 
 import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedOperationParameter;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * @author Spencer Gibb
  */
+@ManagedResource
 public class RefreshBusEndpoint extends AbstractBusEndpoint {
 
 	public RefreshBusEndpoint(ApplicationEventPublisher context, String id,
@@ -19,6 +23,7 @@ public class RefreshBusEndpoint extends AbstractBusEndpoint {
 
 	@RequestMapping(value = "refresh", method = RequestMethod.POST)
 	@ResponseBody
+	@ManagedOperation
 	public void refresh(
 			@RequestParam(value = "destination", required = false) String destination) {
 		publish(new RefreshRemoteApplicationEvent(this, getInstanceId(), destination));
